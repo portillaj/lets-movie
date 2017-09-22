7505762119f6b3a58b38a3c077d9e7a8f926c2c7
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-09-21 20:22:09 -0400

    some styling for section layout and api call successful.  now get search bar working and youtube api for movie trailer is next

--- src/app/index.js
+++ src/app/index.js
@@ -5,12 +5,23 @@ import style from '../scss/main.scss';
 //components
 import Poster from './components/poster-section';
 import Searchbar from './components/search-bar-header';
+import MovieDetails from './components/movie-details';
 
 const MOVIE_API_KEY = '2d733da824cd8c252eab2c2990379324';
 const YOUTUBE_API_KEY =  'AIzaSyAX-BBg9D4Zz5iDgjGuviEIMcsShqVgFmQ';
 const moviePoster = "star wars rogue one";
 
-const url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&language=en-US&query=${moviePoster}&page=1&include_adult=false`;
+const url = `https://api.themoviedb.org/3/movie/330459?api_key=${MOVIE_API_KEY}`;
+
+//`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&language=en-US&query=${moviePoster}&page=1&include_adult=false`; //
+
+//https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+
+
+//`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&language=en-US&query=${moviePoster}&page=1&include_adult=false`;
+
+//`https://api.themoviedb.org/3/movie/330459/latest?api_key=${MOVIE_API_KEY}&language=en-US`
+
 
 class App extends Component {
   constructor(props) {
@@ -25,13 +36,16 @@ render() {
     return  response.json();
 
   }).then(json => {
-    console.log(json);
+    //console.log(json);
     this.setState({
-      trailers: json.results[0].original_title,
-      image: `https://image.tmdb.org/t/p/w500/${json.results[0].poster_path}`,
-      summary: json.results[0].overview,
-      popularity: json.results[0].popularity,
-      release: json.results[0].release_date
+      title: json.original_title,
+      image: `https://image.tmdb.org/t/p/w500/${json.poster_path}`,
+      summary: json.overview,
+      vote: json.vote_average,
+      box: json.revenue,
+      release: json.release_date,
+      runtime: json.runtime,
+      tagline: json.tagline
     });
   }).catch(err => {
     console.log(err)
@@ -39,7 +53,26 @@ render() {
     return (
       <div>
         <Searchbar />
-        <Poster poster={this.state.image}/>
+        <div className="row">
+          <div className="container">
+            <div className="col-md-5">
+                <Poster poster={this.state.image}/>
+            </div>
+            <div className="col-md-7">
+                  <MovieDetails
+                    vote={this.state.vote}
+                    title={this.state.title}
+                    summary={this.state.summary}
+                    release={this.state.release}
+                    box={this.state.box}
+                    runtime={this.state.runtime}
+                    tagline={this.state.tagline}
+                  />
+            </div>
+        </div>
+      </div>
+
+
       </div>
     );
   }
