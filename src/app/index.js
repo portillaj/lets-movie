6274aa86b1119f258bbe8f1639f6edc621f99eab
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-09-29 09:06:07 -0400

    fixed the api call and initial state.  App is fully functional.  Now to add some error handling

--- src/app/index.js
+++ src/app/index.js
@@ -7,9 +7,8 @@ import YTSearch from 'youtube-api-search';
 import Searchbar from './components/search-bar-header';
 import MovieDetails  from './components/movie-details';
 
-
-
-
+const MOVIE_API_KEY = '2d733da824cd8c252eab2c2990379324';
+const YOUTUBE_API_KEY =  'AIzaSyAX-BBg9D4Zz5iDgjGuviEIMcsShqVgFmQ';
 
 class App extends Component {
   constructor(props) {
@@ -27,7 +26,8 @@ class App extends Component {
       runtime: " ",
       tagline: " ",
       backdrop: "",
-      movieTitle: "star%20wars%20rogue%20one"
+      movieTitle: "star wars rogue one",
+      movieId: 330459
     };
 
     //binding function moneyFormat
@@ -44,55 +44,96 @@ class App extends Component {
   //function that updates the state when it is changed in the searchbar component
     inputChange(movieTitle) {
       this.setState({movieTitle})
+    }
+
+fetchMovieApi(url) {
+      fetch(url).then(response => {
+        return  response.json();
+      }).then(json => {
+        console.log(json.results[0].id);
+        //getMovieId(json.results[0].id);
+        this.getMovieId(json.results[0].id);
+      }).catch(err => {
+        console.log(err);
+      });
 
     }
 
-    // let title = this.state.movieTitle;
-    // console.log(`component did mount: ${title}`);
-    // //let url = `https://api.themoviedb.org/3/movie/${this.state.movieId}?&api_key=${MOVIE_API_KEY}`;
-    // this.fetchMovieApi(title);
-
-// call the movie api once when the page is rendered
-//     let url = `https://api.themoviedb.org/3/movie/${this.state.movieId}?&api_key=${MOVIE_API_KEY}`;
-//       let movieTitle = title.replace(/ /g,"%20");
-fetchMovieApi(url) {
-
-      fetch(url).then(response => {
+    getMovieId(id) {
+      let fullUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${MOVIE_API_KEY}&language=en-US`;
+      fetch(fullUrl).then(response => {
         return  response.json();
       }).then(json => {
         console.log(json);
         //setting the state after the api is hit
-        const newMovie = this.setState({
-          title: json.results[0].original_title,
-          image: `https://image.tmdb.org/t/p/w500${json.results[0].poster_path}`,
-          summary: json.results[0].overview,
-          vote: json.results[0].vote_average,
-          box: json.results[0].revenue,
-          release: json.results[0].release_date,
-          runtime: json.results[0].runtime,
-          tagline: json.results[0].tagline,
-          backdrop: json.results[0].backdrop_path,
-          movieId: json.results[0].id,
+         this.setState({
+          title: json.original_title,
+          image: `https://image.tmdb.org/t/p/w500${json.poster_path}`,
+          summary: json.overview,
+          vote: json.vote_average,
+          box: json.revenue,
+          release: json.release_date,
+          runtime: json.runtime,
+          tagline: json.tagline,
+          backdrop: json.backdrop_path,
         });
+        console.log(json.results[0].id);
       }).catch(err => {
         console.log(err);
       });
-    }
+  }
 
 fetchTrailer(youtubeUrl) {
-      //use the youtube api to get the video trailer after the movie api is complete
-
-
       fetch(youtubeUrl).then(response => {
         return response.json();
       }).then(json => {
         this.setState({
           trailer: json.items[0].id.videoId
       });
-
       }).catch(err => {
         console.log(err);
       });
+  }
+
+//when the app first initiates, this is the default movie that is shown
+  componentDidMount() {
+    let url = `https://api.themoviedb.org/3/movie/${this.state.movieId}?api_key=${MOVIE_API_KEY}&language=en-US`;
+    //get url for default movie with movie api
+console.log(url);
+    //get url for default movie trailer
+    let youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${this.state.movieTitle} trailer&key=${YOUTUBE_API_KEY}`;
+    console.log(youtubeUrl);
+
+//fetch the movie star wars rogue one first time app runs
+    fetch(url).then(response => {
+      return  response.json();
+    }).then(json => {
+      //setting the state after the api is hit
+       this.setState({
+        title: json.original_title,
+        image: `https://image.tmdb.org/t/p/w500${json.poster_path}`,
+        summary: json.overview,
+        vote: json.vote_average,
+        box: json.revenue,
+        release: json.release_date,
+        runtime: json.runtime,
+        tagline: json.tagline,
+        backdrop: json.backdrop_path,
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+
+//fetch youtube trailer of star wars rogue one
+    fetch(youtubeUrl).then(response => {
+      return response.json();
+    }).then(json => {
+      this.setState({
+        trailer: json.items[0].id.videoId
+    });
+    }).catch(err => {
+      console.log(err);
+    });
   }
 
 //when the component updated, add the background image from the movie that is being fetched from the api
@@ -105,7 +146,7 @@ render() {
     return (
       <div>
           <div className="container">
-          <Searchbar inputChange={this.inputChange.bind(this)} fetchTrailer={this.fetchTrailer.bind(this)} fetchMovie={this.fetchMovieApi.bind(this)}  />
+          <Searchbar inputChange={this.inputChange.bind(this)} fetchTrailer={this.fetchTrailer.bind(this)} fetchMovie={this.fetchMovieApi.bind(this)} fetchId={this.getMovieId.bind(this)}  />
             <MovieDetails
                 poster={this.state.image}
                 vote={this.state.vote}
